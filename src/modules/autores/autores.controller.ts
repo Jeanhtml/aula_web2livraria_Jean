fa6ce9c122f08ef5a019c1f59f8845be25bbdb8b
Jeanhtml <jean7574@gmail.com>
2026-03-26 22:45:53 -0300

    feat/ novo endpoint deletar autor

--- src/modules/autores/autores.controller.ts
+++ src/modules/autores/autores.controller.ts
@@ -1,5 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { AutoresService } from './autores.service';
+import { AtualizarAutorDto, CriarAutorDto } from './autores.dto';
 
 @Controller('autores')
 export class AutoresController {
@@ -13,5 +23,22 @@ export class AutoresController {
   @Get('/listar-autor/:id')
   listarAutor(@Param('id', ParseIntPipe) id: number) {
     return this.autoresService.listarAutor(id);
+  }
+  @Post('/criar-autor')
+  criarAutor(@Body() bodyRequest: CriarAutorDto) {
+    return this.autoresService.criarAutor(bodyRequest);
+  }
+
+  @Put('atualizar-autor/:id')
+  atualizarautor(
+    @Param('id', ParseIntPipe) idAutor: number,
+    @Body() bodyRequest: AtualizarAutorDto,
+  ) {
+    return this.autoresService.atualizarAutor(idAutor, bodyRequest);
+  }
+
+  @Delete('/deletar-autor/:id')
+  deletarAutor(@Param('id', ParseIntPipe) idAutor: number) {
+    return this.autoresService.deletarAutor(idAutor);
   }
 }
--- src/modules/autores/autores.service.ts
+++ src/modules/autores/autores.service.ts
@@ -1,4 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { AtualizarAutorDto, CriarAutorDto } from './autores.dto';
 
 let autores = [
   {
@@ -21,12 +26,54 @@ let autores = [
 export class AutoresService {
   listarAutores() {
     if (!autores) {
-      return 'Não há autores cadastrados';
+      return 'nao ha autores';
     }
     return autores;
   }
+
   listarAutor(id: number) {
     const autorEncontrado = autores.find((autor) => autor.id === id);
+
+    if (!autorEncontrado) {
+      throw new NotFoundException('Autor não encontrado');
+    }
+
     return autorEncontrado;
+  }
+
+  criarAutor(bodyRequest: CriarAutorDto) {
+    autores.push({
+      id: autores.length + 1,
+      nome: bodyRequest.nome,
+      email: bodyRequest.email,
+    });
+
+    return autores;
+  }
+
+  atualizarAutor(idAutor: number, bodyRequest: AtualizarAutorDto) {
+    const autorEncontrado = this.listarAutor(idAutor);
+
+    if (bodyRequest.nome && !bodyRequest.email) {
+      throw new BadRequestException('Nome e email são obrigatórios');
+    }
+
+    if (bodyRequest.nome) {
+      autorEncontrado.nome = bodyRequest.nome;
+    }
+
+    if (bodyRequest.email) {
+      autorEncontrado.email = bodyRequest.email;
+    }
+
+    return autorEncontrado;
+  }
+
+  deletarAutor(idAutor: number) {
+    this.listarAutor(idAutor);
+
+    autores = autores.filter((autor) => autor.id !== idAutor);
+
+    return autores;
   }
 }
